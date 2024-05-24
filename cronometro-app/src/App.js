f78096564dca8b0739581ad6add693996b071d04
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-05-24 10:24:28 -0300

    Adicionando props

--- cronometro-app/src/App.js
+++ cronometro-app/src/App.js
@@ -4,7 +4,7 @@ import cronometroImg from './assets/cronometro.png';
 function Btn(props) {
   return (
     <>
-      <a></a>
+      <a>{ props.title }</a>
     </>
   );
 }
@@ -15,8 +15,8 @@ function App() {
         <img className='img' src={ cronometroImg } />
         <a className='timer' >0.0</a>
         <div>
-          <Btn />
-          <Btn />
+          <Btn title='Vai' />
+          <Btn title='Limpar' />
         </div>
     </div>
   );
