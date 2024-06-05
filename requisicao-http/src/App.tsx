0cc3ad9bdf63965fb961f402604598700583eafc
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-05 11:11:57 -0300

    Comentando todo codigo

--- requisicao-http/src/App.tsx
+++ requisicao-http/src/App.tsx
@@ -3,42 +3,52 @@ import { useEffect, useState } from 'react';
 import { NutriCard } from './nutriCard';
 import './App.css';
 
-function App() {
 
-  const [nutri, setNutri] = useState<NutriCard[]>([]);
 
-  useEffect( () => {
-    const carregaApi = async (): Promise<void> => {
-      const api: string = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+// [ Código em forma de classe]
 
-      const dados: Response = await fetch(api);
-      const dadosConvertidos: NutriCard[] = await dados.json();
 
-      setNutri(dadosConvertidos);
-    }
-    carregaApi();
-  }, []);
 
-  return (
-    <div>
-      <header>
-        <strong>React Nutri</strong>
-      </header>
+// [ Código em forma de função ]
 
-      {
-        nutri.map((item: NutriCard) => {
-          return (
-            <article className='post' key={ item.id }>
-              <strong className='titulo'>{ item.titulo }</strong>
-              <img className='capa' src={ item.capa } alt={ item.titulo } />
-              <p className='subtitulo'>{ item.subtitulo }</p>
-              <a className='btn'>Acessar</a>
-            </article>
-          );
-        })
-      }
-    </div>
-  )
-}
+// function App() {
 
+//   const [nutri, setNutri] = useState<NutriCard[]>([]);
+
+//   useEffect( () => {
+//     const carregaApi = async (): Promise<void> => {
+//       const api: string = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+//       const dados: Response = await fetch(api);
+//       const dadosConvertidos: NutriCard[] = await dados.json();
+
+//       setNutri(dadosConvertidos);
+//     }
+//     carregaApi();
+//   }, []);
+
+//   return (
+//     <div>
+//       <header>
+//         <strong>React Nutri</strong>
+//       </header>
+
+//       {
+//         nutri.map((item: NutriCard) => {
+//           return (
+//             <article className='post' key={ item.id }>
+//               <strong className='titulo'>{ item.titulo }</strong>
+//               <img className='capa' src={ item.capa } alt={ item.titulo } />
+//               <p className='subtitulo'>{ item.subtitulo }</p>
+//               <a className='btn'>Acessar</a>
+//             </article>
+//           );
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+
+// EXPORT
 export default App;
