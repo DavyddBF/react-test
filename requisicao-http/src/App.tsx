4e96ada11d22a4e20a27e566708a24f256af2f4b
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-05 10:31:45 -0300

    Adicionando interface no codigo

--- requisicao-http/src/App.tsx
+++ requisicao-http/src/App.tsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
+  interface NutriCard {
+    id: number;
+    titulo: string;
+    capa: string;
+    subtitulo: string;
+    categoria: string;
+}
+
+  const [nutri, setNutri] = useState<NutriCard[]>([]);
 
   return (
     <div>
-      
+
     </div>
   )
 }
 
-export default App
+export default App;
