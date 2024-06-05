f265e75f7b4ba5dbea0d14442839fb6259d0a4d3
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-05 10:50:36 -0300

    Criando lógica de trazer dados

--- requisicao-http/src/App.tsx
+++ requisicao-http/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -8,9 +8,21 @@ function App() {
     capa: string;
     subtitulo: string;
     categoria: string;
-}
+  }
 
   const [nutri, setNutri] = useState<NutriCard[]>([]);
+
+  useEffect( () => {
+    const carregaApi= async (): Promise<void> => {
+      const api: string = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+
+      const dados: Response = await fetch(api);
+      const dadosConvertidos: NutriCard[] = await dados.json();
+
+      setNutri(dadosConvertidos);
+    }
+    carregaApi();
+  }, []);
 
   return (
     <div>
