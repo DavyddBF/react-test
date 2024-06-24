dcebce74d00ad3b92d556ac15cd391062f035930
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 16:22:23 -0300

    Adicionando classe

--- oneflix/src/page/Error/index.tsx
+++ oneflix/src/page/Error/index.tsx
@@ -4,12 +4,13 @@ import { Link } from "react-router-dom";
 class Error extends Component {
     public render(): ReactNode {
         return (
-            <div>
+            <div className="erro">
                 <div>
                     <h1>404</h1>
                     <h2>A página "{ document.location.pathname }" não foi encontrada!!</h2>
 
-                    <h3><Link to="/">Voltar para a Home</Link></h3>
+                    <h3>Segue os seguintes links encontrados:</h3>
+                    <Link to='/'>Voltar para a Home</Link>
                 </div>
             </div>
         );    
