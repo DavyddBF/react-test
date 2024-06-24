b8a38eaec4d095dbeeb12b34b0fc2bdee722a26a
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 12:29:44 -0300

    Adicionando a rota Home

---
+++ oneflix/src/router.tsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./page/Home";
+
+
+class RouterApp extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <Home /> } />
+                </Routes>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default RouterApp;
