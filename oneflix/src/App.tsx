374f046ceb1943552e744bb4020d3012028c2471
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 12:07:00 -0300

    Criando classe

---
+++ oneflix/src/App.tsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+
+class App extends Component {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default App;
