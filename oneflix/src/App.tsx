651ce05ec8fa64657ecac3cf70636e1623bba3af
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 12:40:59 -0300

    Adicionando a rota no App

--- oneflix/src/App.tsx
+++ oneflix/src/App.tsx
@@ -1,18 +1,17 @@
-import { Component } from "react";
+import { Component, ReactNode } from "react";
+import RouterApp from "./router";
 
 class App extends Component {
     constructor(props: any) {
         super(props);
         this.state = {
-            
+
         };
     }
 
-    render() {
+    public render(): ReactNode {
         return (
-            <div>
-
-            </div>
+            <RouterApp />
         );
     }
 }
