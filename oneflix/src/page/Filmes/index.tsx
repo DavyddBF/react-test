ae5f45839ee254f4860ad77060b733647c9f8316
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 18:41:06 -0300

    Criando componente de filmes

---
+++ oneflix/src/page/Filmes/index.tsx
@@ -0,0 +1,11 @@
+import { Component, ReactNode } from "react";
+
+class Filmes extends Component {
+    public render(): ReactNode {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default Filmes;
