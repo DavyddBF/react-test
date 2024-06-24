c9bb777cd0f15fb292342088e452a1044d6d65aa
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-24 16:52:38 -0300

    Criando a lógica de buscar informações da api

--- oneflix/src/page/Home/index.tsx
+++ oneflix/src/page/Home/index.tsx
@@ -1,6 +1,40 @@
+import { AxiosResponse } from "axios";
 import { Component, ReactNode } from "react";
+import api from "../../services/api";
+import FilmeType from "../../filme";
 
-class Home extends Component {
+interface ComponenteState {
+    filmes: FilmeType[],
+    loading: boolean
+}
+
+class Home extends Component<{}, ComponenteState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            filmes: [],
+            loading: true
+        };
+
+        this.componentDidMount = this.componentDidMount.bind(this);
+    }
+
+    async componentDidMount(): Promise<void> {
+        const resposta: AxiosResponse<any, any> = await api.get("movie/now_playing", {
+            params: {
+                api_key: '184ee90db92bf2330058b37260977a7f',
+                language: 'pt-BR',
+                page: 1
+            }
+        });
+
+        const resultados: FilmeType[] = resposta.data.results;
+        this.setState({
+            filmes: resultados,
+            loading: false
+        });
+    }
+
     public render(): ReactNode {
         return (
             <div>
