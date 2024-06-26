89de539517f6edd8087639b3006e7ade16e30b06
Davyd Ferreira <100801538+DavyddBF@users.noreply.github.com>
2024-06-26 17:21:24 -0300

    Criando lógica de requisição do conteudo

--- oneflix/src/page/Filmes/index.tsx
+++ oneflix/src/page/Filmes/index.tsx
@@ -1,6 +1,42 @@
 import { Component, ReactNode } from "react";
+import FilmeType from "../../filme";
+import api from "../../services/api";
+import { useParams } from "react-router-dom";
 
-class Filmes extends Component {
+interface ComponenteState {
+    filme: FilmeType[],
+    loading: boolean
+}
+
+class Filmes extends Component<{}, ComponenteState> {
+    public id;
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            filme: [],
+            loading: true
+        };
+
+        this.id = useParams();
+    }
+
+    public async componentDidMount(): Promise<void> {
+        await api.get(`movie/${ this.id }`, {
+            params: {
+                api_key: '184ee90db92bf2330058b37260977a7f',
+                language: 'pt-BR'
+            }
+        }).then( resposta => {
+            this.setState({
+                filme: resposta.data,
+                loading: false
+            })
+        }).catch( () => {
+            console.log("Erro");
+        });
+    }
+
     public render(): ReactNode {
         return (
             <div></div>
