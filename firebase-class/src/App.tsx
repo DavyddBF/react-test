767384044668e6e9d589253f034815df99acafc0
DavyddBF <davydextreme460@gmail.com>
2024-07-13 23:31:32 -0300

    Criado interface, states e render

--- firebase-class/src/App.tsx
+++ firebase-class/src/App.tsx
@@ -1,16 +1,36 @@
 import { Component } from 'react';
 import './App.css';
 
+interface Users {
+  id: string,
+  user: string,
+  idade: string
+}
+
 interface AppState {
-  
+  user: string,
+  idade: string,
+  email: string,
+  senha: string,
+  users: Users[]
 }
 
 class App extends Component<{}, AppState>  {
   constructor(props: any) {
     super(props);
     this.state = {
-
+      user: '',
+      idade: '',
+      email: '',
+      senha: '',
+      users: []
     }
+  }
+
+  public render(): JSX.Element {
+    return (
+      <div></div>
+    );
   }
 }
 
