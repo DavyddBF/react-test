bf579682025da0ca2328497cc6beb42c7ab232b5
DavyddBF <davydextreme460@gmail.com>
2024-07-13 23:26:47 -0300

    Atualizando arquivos

---
+++ firebase-class/src/App.tsx
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './App.css';
+
+interface AppState {
+  
+}
+
+class App extends Component<{}, AppState>  {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+
+    }
+  }
+}
+
+export default App;
