3e6c32a08156228738daee64a25f82560d7f55af
DavyddBF <davydextreme460@gmail.com>
2024-07-15 12:14:00 -0300

    Adicionando cadastro de users e idade

--- firebase-class/src/App.tsx
+++ firebase-class/src/App.tsx
@@ -41,17 +41,25 @@ class App extends Component<{}, AppState>  {
   public async novoUsuario(): Promise<void> {
     await createUserWithEmailAndPassword(auth, this.state.email, this.state.senha)
     .then(() => {
-      console.log('Cadastrado com sucesso!!!');
-      this.setState({ email: ''});
-      this.setState({ senha: ''});
-  })
-  .catch((erro) => {
-      if(erro.code == 'auth/weak-password'){
-          alert('Senha muito fraca!!');
-      } else if (erro.code == 'auth/email-already-in-use') {
-          alert('Email já existe!!');
-      }
-  });
+        console.log('Cadastrado com sucesso!!!');
+        this.setState({ email: ''});
+        this.setState({ senha: ''});
+    })
+    .catch((erro) => {
+        if(erro.code == 'auth/weak-password'){
+            alert('Senha muito fraca!!');
+        } else if (erro.code == 'auth/email-already-in-use') {
+            alert('Email já existe!!');
+        }
+    });
+  }
+
+  public async cadastrarUser(): Promise<void> {
+    
+  }
+
+  public async buscarTodosUsers(): Promise<void> {
+
   }
 
   public render(): JSX.Element {
@@ -66,7 +74,7 @@ class App extends Component<{}, AppState>  {
                 type="email" 
                 placeholder='Insira seu email'
                 value={ this.state.email }
-                onChange={ (evento) => this.setState({ email: evento.target.value}) }
+                onChange={ (evento) => this.setState({ email: evento.target.value }) }
             />
 
             <label>Senha:</label>
@@ -74,11 +82,33 @@ class App extends Component<{}, AppState>  {
                 type="password" 
                 placeholder='Insira sua senha'
                 value={ this.state.senha }
-                onChange={ (evento) => this.setState({ senha: evento.target.value}) }
+                onChange={ (evento) => this.setState({ senha: evento.target.value }) }
             />
             <br/>
             <button onClick={ this.novoUsuario }>Cadastrar</button>
         </div>
+        <div className='container'>
+                <h2>Cadastro User e Idade</h2>
+                <label>User:</label>
+                <input 
+                    type='text' 
+                    placeholder='Digite o seu nome/user'
+                    value={ this.state.user }
+                    onChange={ (evento) => this.setState({ user: evento.target.value }) }
+                />
+
+                <label>Idade:</label>
+                <input 
+                    type="text" 
+                    placeholder='Digite sua idade'
+                    value={ this.state.idade }
+                    onChange={ (evento) => this.setState({ idade: evento.target.value }) }
+                /> <br/>
+
+                <button className='btn' onClick={ this.cadastrarUser }>Cadastrar</button>
+                <button className='btn' onClick={ this.buscarTodosUsers }>Buscar usuários</button> <br/>
+
+            </div>
       </div>
     );
   }
